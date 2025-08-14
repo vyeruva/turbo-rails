75c8d605772efb13be100fd179348ac1492dbc7e
Vineeth Yeruva <reddyshack@gmail.com>
2025-08-14 07:27:20 -0600

    finished realtime updates with turbo streams chapter

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,5 +3,9 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
