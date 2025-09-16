8685b426c123fdb58f2b28f8b5b8ec8d54b93a44
Vineeth Yeruva <reddyshack@gmail.com>
2025-09-16 08:00:30 -0600

    chapter 11 done!

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,10 +1,15 @@
 class Quote < ApplicationRecord
   belongs_to :company
   has_many :line_item_dates, dependent: :destroy
+  has_many :line_items, through: :line_item_dates
 
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
 
   # after_create_commit -> { broadcast_prepend_later_to "quotes" }
   # after_update_commit -> { broadcast_prepend_later_to "quotes" }
